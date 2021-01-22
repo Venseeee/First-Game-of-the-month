c9ea7812879c99dc739bca9d9e1ae2a3a4acf82a
vense <vense.lrm@gmail.com>
2021-01-22 13:57:11 +0100

    rat

---
+++ Assets/EndZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndZone : MonoBehaviour
+{
+    public string GameOver;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player");
+        ChangeScene();
+    }
+    
+}
+
